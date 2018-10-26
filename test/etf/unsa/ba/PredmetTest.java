dc56458ef76bc20add81ab127af244375d8c177f
mplakalovi1 <mplakalovi1@etf.unsa.ba>
2018-10-26 11:12:01 +0200

    dodani testovi za class Predmet(treba ih definisati)

---
+++ test/etf/unsa/ba/PredmetTest.java
@@ -0,0 +1,40 @@
+package etf.unsa.ba;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PredmetTest {
+
+    @Test
+    void upisi() {
+    }
+
+    @Test
+    void ispisi() {
+    }
+
+    @Test
+    void getNazivPredmeta() {
+    }
+
+    @Test
+    void getSifraPredmeta() {
+    }
+
+    @Test
+    void getMaxBrojStudenata() {
+    }
+
+    @Test
+    void setNazivPredmeta() {
+    }
+
+    @Test
+    void setSifraPredmeta() {
+    }
+
+    @Test
+    void ispisSpiskaStudenata() {
+    }
+}
